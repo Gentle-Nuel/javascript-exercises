daccca30624df253b4df615f2371978d74e433cf
Gentle-Nuel <embaeri@gmail.com>
2023-06-05 11:39:32 +0100

    Add a function that removes an arg from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+let newArray = Array.from(arr).filter(element =>!args.includes(element));
+return newArray;
 };
+
+console.log([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
